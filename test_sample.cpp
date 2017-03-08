cd3c403336c7763b16bf494b4a3ad3f61ca956ec
kentrino <kentrino@h-kento.jp>
2017-03-08 19:31:44 +0900

    add test?

---
+++ test_sample.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+int main(int argc, const char *argv[]) {
+    printf("Hello testing\n");
+    return 1;
+}
